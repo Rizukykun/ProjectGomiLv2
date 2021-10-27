e267c2773daf6da09d133caa0f1711bd70699597
Daelfin <fabioarraisgomes@hotmail.com>
2021-10-27 13:44:14 -0300

    Servlets de Cadastro de Cliente e Motorista

---
+++ src/main/java/br/com/gomi/api/MotoristaServlet.java
@@ -0,0 +1,42 @@
+package br.com.gomi.api;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Motorista")
+public class MotoristaServlet extends PadraoServlet {
+	private static final long serialVersionUID = 1L;
+
+	public MotoristaServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		int sc;
+		String textResponse = "";
+
+		try {
+			CadastraUsuario(request, false);
+			sc = HttpServletResponse.SC_OK;
+		} catch (Exception e) {
+			sc = HttpServletResponse.SC_BAD_REQUEST;
+			response.getWriter().write(e.getMessage());
+			response.getWriter().flush();
+		}
+
+		response.setStatus(sc);
+
+		if (!textResponse.equals("")) {
+			response.getWriter().write(textResponse);
+			response.getWriter().flush();
+		}
+	}
+
+}
